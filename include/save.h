63617480193bc70b15a9bcb0c8e63b15ce893d88
Gabriel Cowley <gabrielcowley@yahoo.co.uk>
2023-05-13 15:29:15 +0100

    Free up some space in saveblocks

--- include/save.h
+++ include/save.h
@@ -1,10 +1,10 @@
 #ifndef GUARD_SAVE_H
 #define GUARD_SAVE_H
 
-// Each 4 KiB flash sector contains 3968 bytes of actual data followed by a 128 byte footer.
-// Only 12 bytes of the footer are used.
-#define SECTOR_DATA_SIZE 3968
-#define SECTOR_FOOTER_SIZE 128
+// Unused space has been moved into DATA from FOOTER
+// This gives an extra 116 bytes in SaveBlock2, an extra 464 bytes in SaveBlock1, and an extra 1044 bytes in PokemonStorage
+#define SECTOR_DATA_SIZE 4084
+#define SECTOR_FOOTER_SIZE 12
 #define SECTOR_SIZE (SECTOR_DATA_SIZE + SECTOR_FOOTER_SIZE)
 
 #define NUM_SAVE_SLOTS 2
