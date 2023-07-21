476ba7d8c33a24597cb2cbe1bebc77370096f2d5
Gabriel Cowley <gabrielcowley@yahoo.co.uk>
2023-07-22 00:34:00 +0100

    Nickname from party menu
    Credit to Shinny456, TheXaman, Lunos and Zadien

--- src/strings.c
+++ src/strings.c
@@ -1832,6 +1832,7 @@ const u8 gText_Answer[] = _("Answer");
 const u8 gText_PokeBalls[] = _("Poké Balls");
 const u8 gText_Berry[] = _("Berry");
 const u8 gText_Berries[] = _("Berries");
+const u8 gText_Nickname[] = _("Nickname");
 
 //// side quest data
 //names
